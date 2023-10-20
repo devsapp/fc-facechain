c9fcad14048a75b282aca8aa07c9a28480aff80c
OhYee <oyohyee@oyohyee.com>
2023-10-20 17:25:31 +0800

    dev

--- src/initial/entrypoint.sh
+++ src/initial/entrypoint.sh
@@ -8,4 +8,5 @@ files="$(curl "${URL}/files.list")"
 # 提前创建文件夹，避免由于并发创文件夹导致报错
 echo "$files" | xargs -I {} dirname {} | xargs -I {} mkdir -p "${TARGET}/{}"
 
-echo "$files" | xargs -P256 -I {} ./aria2c -x 256 -s 256 --allow-overwrite=true -c "${URL}/{}" --dir "${TARGET}"  --out "{}"
+# echo "$files" | xargs -P256 -I {} ./aria2c -x 256 -s 256 --allow-overwrite=true -c "${URL}/{}" --dir "${TARGET}"  --out "{}"
+echo "$files" | xargs -P256 -I {} mwget "${URL}/{}" -n 64 -f "{}"
